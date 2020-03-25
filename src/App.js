96341986256d21053dc3ac27486dbe63e0bc8f9e
rodrigo <fcorodrigo.ti@gmail.com>
2020-03-25 20:06:42 -0300

    Front-End do App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './routes'
+
+import './global.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <strong>Hello OmniStack 11</strong>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
